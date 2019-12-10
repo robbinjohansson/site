ab99fd01f1675bfc6812aaf6d80660883b571ab6
Robbin Johansson <mail@robbin.works>
2019-12-10 08:53:13 +0100

    Add custom App.vue

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <MainLayout>
+    <router-view />
+  </MainLayout>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+    siteDescription
+  }
+}
+</static-query>
+
+<script>
+import MainLayout from '~/layouts/Default.vue';
+
+export default {
+  metaInfo() {
+    return {
+      title: this.$static.metadata.siteName,
+      meta: [
+        {
+          key: 'description',
+          name: 'description',
+          content: this.$static.metadata.siteDescription
+        }
+      ]
+    }
+  },
+
+  components: {
+    MainLayout,
+  },
+}
+</script>
