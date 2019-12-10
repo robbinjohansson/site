513d78eb67df9a3550db0e76e501c5dad8e0e13e
Robbin Johansson <mail@robbin.works>
2019-12-10 09:15:36 +0100

    Add purgecss

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,4 +1,11 @@
-const tailwindcss = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss');
+const tailwind = require('tailwindcss');
+
+const postcssPlugins = [
+  tailwind('./tailwind.config.js'),
+]
+
+if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss());
 
 module.exports = {
   siteName: 'robbin.works',
@@ -8,10 +15,8 @@ module.exports = {
   css: {
     loaderOptions: {
       postcss: {
-        plugins: [
-          tailwindcss
-        ],
+        plugins: postcssPlugins,
       },
     },
-  }
+  },
 }
