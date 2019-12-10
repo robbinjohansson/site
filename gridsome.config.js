01f163a63ea787f71739f9168c4a1d3d317849b3
Robbin Johansson <mail@robbin.works>
2019-12-10 09:06:27 +0100

    Add tailwind css

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,17 @@
-// This is where project configuration and plugin options are located. 
-// Learn more: https://gridsome.org/docs/config
-
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
+const tailwindcss = require('tailwindcss');
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'robbin.works',
+  siteDescription: 'My name is Robbin, I enjoy building great web apps.',
+  siteUrl: 'https://robbin.works/',
+  plugins: [],
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          tailwindcss
+        ],
+      },
+    },
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-export default function (Vue, { router, head, isClient }) {}
+import "./assets/css/app.css";
+
+export default function (Vue, { router, head, isClient }) {
+  head.htmlAttrs = { lang: 'en', class: 'bg-white antialiased' }
+
+  head.bodyAttrs = { class: 'font-sans text-base font-normal tracking-tight leading-snug text-gray-800' }
+}
