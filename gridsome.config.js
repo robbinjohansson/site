48c6bc50dbec81d47845bcbc2c55da231ae4054f
Robbin Johansson <mail@robbin.works>
2019-12-12 20:22:58 +0100

    Add GA

--- gridsome.config.js
+++ gridsome.config.js
@@ -33,6 +33,12 @@ module.exports = {
           }
         }
       }
+    },
+    {
+    use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: 'UA-134187327-1'
+      }
     }
   ],
 
