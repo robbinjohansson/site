a897b1ba067c3f65a11087dcafb4e8aa6c52b812
Robbin Johansson <mail@robbin.works>
2020-01-10 21:46:01 +0100

    shared ArticleList component

---
+++ src/components/ArticleList.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+      <h1 class="mb-3">{{ headerText }}</h1>
+
+      <div :class="{'border-b mb-5 pb-10': pageInfo.totalPages > 1}">
+        <div
+          v-for="edge in collection"
+          :key="edge.node.id"
+          class="mb-6 flex flex-col items-start"
+        >
+
+          <span class="text-sm text-gray-600 mb-2">
+            <span>{{ edge.node.date }} // <span v-if="edge.node.sharable_domain" v-html="`[${edge.node.sharable_domain}]`"></span><span v-else>{{ edge.node.timeToRead }} min read</span></span>
+          </span>
+
+          <a
+            v-if="edge.node.sharable_url"
+            :href="edge.node.sharable_url"
+            target="_blank"
+            rel="nofollow noopener noreferrer"
+          >
+            {{ edge.node.title }}
+          </a>
+
+          <g-link
+            v-else
+            :to="edge.node.path"
+          >
+            {{ edge.node.title }}
+          </g-link>
+
+        </div>
+      </div>
+
+      <Pager class="nav" v-if="pageInfo.totalPages > 1" :info="pageInfo"/>
+
+    </div>
+</template>
+
+<script>
+import { Pager } from 'gridsome'
+
+export default {
+    props: ['headerText', 'pageInfo', 'collection'],
+
+    components: {
+      Pager
+    },
+}
+</script>
--- src/pages/Archive.vue
+++ src/pages/Archive.vue
@@ -1,44 +1,14 @@
 <template>
-  <div>
-    <h1 class="mb-3">Archive</h1>
-
-    <div :class="{'border-b mb-5 pb-10': $page.posts.pageInfo.totalPages > 1}">
-      <div
-        v-for="edge in $page.posts.edges"
-        :key="edge.node.id"
-        class="mb-6 flex flex-col items-start"
-      >
-
-        <span class="text-sm text-gray-600 mb-2">
-          <span>{{ edge.node.date }} // <span v-if="edge.node.sharable_domain" v-html="`[${edge.node.sharable_domain}]`"></span><span v-else>{{ edge.node.timeToRead }} min read</span></span>
-        </span>
-
-        <a
-          v-if="edge.node.sharable_url"
-          :href="edge.node.sharable_url"
-          target="_blank"
-          rel="nofollow noopener noreferrer"
-        >
-          {{ edge.node.title }}
-        </a>
-
-        <g-link
-          v-else
-          :to="edge.node.path"
-        >
-          {{ edge.node.title }}
-        </g-link>
-
-      </div>
-    </div>
-
-    <Pager class="nav" v-if="$page.posts.pageInfo.totalPages > 1" :info="$page.posts.pageInfo"/>
-
-  </div>
+  <article-list
+    headerText="Archive"
+    :pageInfo="$page.posts.pageInfo"
+    :collection="$page.posts.edges"
+  >
+  </article-list>
 </template>
 
 <script>
-import { Pager } from 'gridsome'
+import ArticleList from '~/components/ArticleList';
 
 export default {
   metaInfo: {
@@ -46,8 +16,8 @@ export default {
   },
 
   components: {
-    Pager
-  }
+    'article-list': ArticleList,
+  },
 }
 </script>
 
